e2a272977172503ebf35e18a4c507d106cf2854d
roto17 <zerouali.khalid2@gmail.com>
2024-09-11 20:23:29 +0100

    set Env

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/roto17/zeus
+
+go 1.23.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("dbport"))
 }
