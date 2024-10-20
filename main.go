56315ffab379271b54a1749f640bc0ed9773466e
roto17 <zerouali.khalid2@gmail.com>
2024-10-20 17:33:53 +0100

    clean main

--- main.go
+++ main.go
@@ -8,24 +8,12 @@ import (
 )
 
 func main() {
-	// Setup PostgreSQL connection
+
+	// Load Config
 	config.LoadConfig()
 
-	// lang := os.Getenv("LANG")
-	// if lang == "" {
-	// 	lang = "en" // Default to English
-	// }
-
-	// Initialize the database
+	// Initialize and load the database
 	database.InitDB()
-
-	// Auto-migrate the models
-	// err := database.DB.AutoMigrate(&models.User{}, &models.Token{})
-	// if err != nil {
-	// 	log.Fatal("failed to migrate the database:", err)
-	// }
-
-	// Initialize Gin router
 
 	// Run the server
 	router.InitRouter().Run(config.GetEnv("app_running_port")) // Default is :8080
